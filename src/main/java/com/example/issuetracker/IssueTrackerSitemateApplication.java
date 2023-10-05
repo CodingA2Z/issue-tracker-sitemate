fa5fc4c673260d17cde0d4978c502cc9ca98bafe
Subhasis.Mallick <subhasis.mallick@singtel.com>
2023-10-05 16:44:06 +0800

    project added

---
+++ src/main/java/com/example/issuetracker/IssueTrackerSitemateApplication.java
@@ -0,0 +1,13 @@
+package com.example.issuetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueTrackerSitemateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueTrackerSitemateApplication.class, args);
+	}
+
+}
